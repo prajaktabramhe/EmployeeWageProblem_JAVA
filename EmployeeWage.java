dff8c45c04df5823c95b0b1b75a66b233f430859
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-03-12 22:19:22 +0530

    [Prajakta]:Computed Employee Wage and Save Total Wage by Company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,16 +1,28 @@
-import java.util.Scanner;
+
  public class EmployeeWage {
        public static final int IS_PART_TIME = 1;
        public static final int IS_FULL_TIME = 2;
+        
 
-	public static int computeEmpWage(String company, int empRatePerHour ,int  numOfWorkingDays , int maxHoursPerMonth)
+       private final String company;
+       private final int empRatePerHour;
+       private final int numOfWorkingDays;
+       private final int maxHoursPerMonth;
+       private int totalEmpWage;
+
+       public EmployeeWage(String company, int empRatePerHour ,int  numOfWorkingDays , int maxHoursPerMonth)
 	{
 
-          //Variables
-          int empHrs = 0;
-          int totalEmpHrs = 0;
-          int totalWorkingDays = 0;
-          
+          this.company = company;
+          this.empRatePerHour = empRatePerHour;
+          this.numOfWorkingDays = numOfWorkingDays;
+          this.maxHoursPerMonth =  maxHoursPerMonth;
+        }
+       public void computeEmpWage() 
+       {
+          //variables
+           int empHrs = 0,totalEmpHrs = 0, totalWorkingDays = 0;
+ 
           //Computation
           while (totalEmpHrs <= maxHoursPerMonth && 
                  totalWorkingDays < numOfWorkingDays)
@@ -31,16 +43,22 @@ import java.util.Scanner;
                  totalEmpHrs += empHrs;
                  System.out.println("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs);
            }
-           int totalEmpWage = totalEmpHrs * empRatePerHour;
-           System.out.println("Total Emp Wage for company: " + company+" is:" + totalEmpWage);
-           return totalEmpWage;
+            totalEmpWage = totalEmpHrs * empRatePerHour;
+
 
 
       }
-	public static void main(String args[])
-	{
-		computeEmpWage("DMart", 20, 2, 10);
-                computeEmpWage("Reliance", 10, 4, 20);
+	
+      public String toString() {
+      return "Total Emp Wage for Company: " + company +" is: " + totalEmpWage;
+      }
+      public static void main(String[] args) {
 
+            EmployeeWage dMart = new EmployeeWage("DMart", 20, 2, 10);
+            EmployeeWage reliance = new EmployeeWage("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            System.out.println(dMart);
+            reliance.computeEmpWage();
+            System.out.println(reliance);
         }
 }
